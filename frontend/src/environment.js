10395bdb61e69c237376963b88be3e35054779f3
Vamshi Yadav <mekalavamshiyadav46@gmail.com>
2025-10-14 20:51:51 +0530

    deployment changes

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
-// const server = IS_PROD ?
-//     "https://joinIn-backend.onrender.com" :
+const server = IS_PROD ?
+    "https://joinin-backend.onrender.com" :
+    "http://localhost:8000"
 
-//     "http://localhost:8000"
 
-const server = "http://localhost:8000"
 export default server;
